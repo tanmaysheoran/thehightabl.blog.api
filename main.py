ecfd18cee9df74c054dd51d1e2a77672f9df3b0e
= <=>
2024-09-15 13:37:16 +0530

    reverted main changes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from routes.Post import router as post_router
 from routes.EmailTemplate import router as email_template_router
 import os
 
-
+# MongoDB connection string (update with your credentials if necessary)
 MONGO_URI = os.environ.get("MONGO_URI")
 API_KEY = "mysecretapikey123"
 
@@ -26,6 +26,7 @@ async def lifespan(app: FastAPI):
     client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
     database = client.get_database(name="blog")
     await init_beanie(database, document_models=[NewsletterSignup, Post, BlogContent, EmailTemplate])
+    yield
 
 # Run the database initialization on startup
 app = FastAPI(lifespan=lifespan)
