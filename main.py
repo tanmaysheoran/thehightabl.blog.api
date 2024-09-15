61b24418d4ce9a2ead9cf8a955954e76d481d9cd
= <=>
2024-09-15 16:37:59 +0530

    changed the way db is initialized added template folder for html added home page html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from beanie import init_beanie
 import motor.motor_asyncio
@@ -20,16 +21,24 @@ import os
 MONGO_URI = os.environ.get("MONGO_URI")
 API_KEY = "mysecretapikey123"
 
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+database = client.get_database(name="blog")
+
+##This is to be used when deploying via dedicated server, does not work with function apps like vercel
+
 # Connect to the database and initialize Beanie with the models
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-    database = client.get_database(name="blog")
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await init_beanie(database, document_models=[NewsletterSignup, Post, BlogContent, EmailTemplate])
+#     yield
+
+async def initialize_database():
+    """Ensures that Beanie is initialized with the database"""
     await init_beanie(database, document_models=[NewsletterSignup, Post, BlogContent, EmailTemplate])
-    yield
 
 # Run the database initialization on startup
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 origins = ["https://journey.thehightabl.com", "http://localhost:3000"]
 
@@ -41,6 +50,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def db_init_middleware(request: Request, call_next):
+    await initialize_database()
+    response = await call_next(request)
+    return response
+
 # API endpoint to sign up for the newsletter
 app.include_router(blog_content_router)
 app.include_router(geolocation_router)
@@ -48,6 +63,12 @@ app.include_router(newsletter_router)
 app.include_router(post_router)
 app.include_router(email_template_router)
 
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    with open("html_templates/home.html", "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content)
 
 # To run the FastAPI app, use: uvicorn app_name:app --reload
 if __name__ == '__main__':
