02ce916f2d591ccf37dab48f23276e0b723a62a7
= <=>
2024-09-17 21:43:23 +0530

    updated condition on signup

--- routes/Newsletter.py
+++ routes/Newsletter.py
@@ -3,7 +3,7 @@ from typing import List
 from pydantic import EmailStr,BaseModel
 from fastapi import Security,APIRouter, HTTPException
 from fastapi.responses import HTMLResponse
-
+import logging
 from classes.APIKey import get_api_key
 from classes.Post import Post
 from classes.NewsLetterSignup import NewsletterSignup
@@ -22,28 +22,33 @@ class NewsletterSignupRequest(BaseModel):
 
 router = APIRouter(prefix="/newsletter", tags=["Newsletter"])
 
-@router.post("/signup", response_model=NewsletterSignup, status_code=201)
+@router.post("/signup", response_model=bool, status_code=201)
 async def signup_for_newsletter(newsletter: NewsletterSignupRequest):
-    welcome_mail_template_id = os.environ.get("NEWSLETTER_WELCOME_EMAIL_TEMPLATE_ID")
-    if not welcome_mail_template_id:
-        raise HTTPException(status_code=500, detail="Missing email template ID in environment variables")
-    
-    email_template = await EmailTemplate.get(welcome_mail_template_id)
-    
-    existing_user = await NewsletterSignup.find_one(NewsletterSignup.email == newsletter.email)
-    if existing_user and existing_user.isActive:
-        raise HTTPException(status_code=400, detail="Email already signed up for the newsletter")
-    elif not existing_user.isActive:
-        existing_user.isActive = True
-        await existing_user.save()
-    else:
-        new_signup = NewsletterSignup(**newsletter.model_dump())
-        await new_signup.insert()
+    try:
+        welcome_mail_template_id = os.environ.get("NEWSLETTER_WELCOME_EMAIL_TEMPLATE_ID")
+        if not welcome_mail_template_id:
+            raise HTTPException(status_code=500, detail="Missing email template ID in environment variables")
+        
+        email_template = await EmailTemplate.get(welcome_mail_template_id)
+        
+        existing_user = await NewsletterSignup.find_one(NewsletterSignup.email == newsletter.email)
+        if existing_user:
+            if existing_user.isActive:
+                raise HTTPException(status_code=400, detail="Email already signed up for the newsletter")
+            else:
+                existing_user.isActive = True
+                await existing_user.save()
+        else:
+            new_signup = NewsletterSignup(**newsletter.model_dump())
+            await new_signup.insert()
 
-    send_grid = SendGrid()
-    send_grid.send_email(to_email=newsletter.email, subject=email_template.subject,content=email_template.body.replace("[name]", newsletter.name))
+        send_grid = SendGrid()
+        send_grid.send_email(to_email=newsletter.email, subject=email_template.subject,content=email_template.body.replace("[name]", newsletter.name))
 
-    return True
+        return True
+    except Exception as e:
+        logging.error(f"Error occured while executing signup_for_newsletter : {e}")
+        raise HTTPException(status_code=500, detail=f"Error occured while executing signup_for_newsletter : {e}")
 
 @router.get("/users", response_model=List[NewsletterSignup])
 async def get_newsletter_users(api_key = Security(get_api_key)):
